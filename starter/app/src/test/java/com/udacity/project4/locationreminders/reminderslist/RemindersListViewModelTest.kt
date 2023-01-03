2da1dfaa9265c8a46a995e5ccac63757ebdfe37c
mohamedebrahem13 <3_202215576@alexu.edu.eg>
2023-01-03 21:14:17 +0200

    create live data test util add  main coroutines rule to switch thread so we can test in main thread add fake data source in RemindersListViewModelTest check for the error live data snackbar check for showloading live data add fake data source in ReminderViewModelTest add two test methode so we can text if location is validate by check the value of it's title and location str

--- starter/app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ starter/app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -1,28 +1,28 @@
 package com.udacity.project4.locationreminders.reminderslist
 
-import android.app.Application
 import android.os.Build
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.udacity.project4.locationreminders.MainCoroutineRule
 import com.udacity.project4.locationreminders.data.FakeDataSource
-import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
+import com.udacity.project4.locationreminders.getOrAwaitValue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.*
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
 import org.robolectric.annotation.Config
 
 @Config(sdk = [Build.VERSION_CODES.P])
 // Set the main coroutines dispatcher for unit testing.
-
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
 class RemindersListViewModelTest {
+    // Executes each task synchronously using Architecture Components.
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
 
@@ -31,81 +31,55 @@ class RemindersListViewModelTest {
     var mainCoroutineRule = MainCoroutineRule()
 
     // Subject under test
-    private lateinit var saveReminderViewModel: SaveReminderViewModel
+    private lateinit var remindersListViewModel: RemindersListViewModel
 
     // Use a fake repository to be injected into the view model.
     private lateinit var fakeDataSource: FakeDataSource
-
     @Before
     fun setupViewModel() {
+        stopKoin()
         // Initialise the repository with no reminders.
         fakeDataSource = FakeDataSource()
-        val appContext = ApplicationProvider.getApplicationContext() as Application
-        saveReminderViewModel = SaveReminderViewModel(appContext, fakeDataSource)
-    }
 
-    @Test
-    fun whenIncompleteInfo_validationReturnsTrue() {
-        // GIVEN
-        //  reminder fields if location and title  not null should return false
-        saveReminderViewModel.onClear()
-        saveReminderViewModel.reminderTitle.value = "location"
-        saveReminderViewModel.reminderDescription.value = "some description"
-        saveReminderViewModel.reminderSelectedLocationStr.value = "location"
-        saveReminderViewModel.longitude.value = 20.0
-        saveReminderViewModel.latitude.value = 10.0
-
-        // WHEN - attempting to validate
-        val result = saveReminderViewModel.validateEnteredData(
-            ReminderDataItem(
-                saveReminderViewModel.reminderTitle.value,
-                saveReminderViewModel.reminderDescription.value,
-                saveReminderViewModel.reminderSelectedLocationStr.value,
-                saveReminderViewModel.longitude.value,
-                saveReminderViewModel.latitude.value,
-                "2"
-            )
+        // using fake data source in ListViewModel by inject because it's test double
+        // becouse we don't have lifecycle observer we use live data util (use life observeForever)
+        remindersListViewModel = RemindersListViewModel(
+            ApplicationProvider.getApplicationContext(), fakeDataSource
         )
-
-        // THEN - result is false
-        // using hamcrest is for more readability
-        assertThat(result, `is`(true))
-
     }
 
 
 
     @Test
-    fun whenIncompleteInfo_validationReturnsNull() {
-        // GIVEN
-        //  reminder fields if location  is null should return false
-        saveReminderViewModel.onClear()
-        saveReminderViewModel.reminderTitle.value = "location"
-        saveReminderViewModel.reminderDescription.value = "some description"
-        saveReminderViewModel.reminderSelectedLocationStr.value = null
-        saveReminderViewModel.longitude.value = 15.0
-        saveReminderViewModel.latitude.value = 10.0
+    fun sandbarError() {
+        // GIVEN - there's a problem loading reminders
+        // Make the repository return errors so the snack is not null
+        fakeDataSource.setReturnError(true)
 
-        // WHEN - attempting to validate
-        val result = saveReminderViewModel.validateEnteredData(
-            ReminderDataItem(
-                saveReminderViewModel.reminderTitle.value,
-                saveReminderViewModel.reminderDescription.value,
-                saveReminderViewModel.reminderSelectedLocationStr.value,
-                saveReminderViewModel.longitude.value,
-                saveReminderViewModel.latitude.value,
-                "1"
-            )
-        )
+        // WHEN - we want to load rhe reminders
+        remindersListViewModel.loadReminders()
 
-        // THEN - result is false
+        // THEN - It's an error, there's a snackBar
         // using hamcrest is for more readability
-        assertThat(result, `is`(false))
 
+        assertThat(remindersListViewModel.showSnackBar.getOrAwaitValue(), not(nullValue()))
     }
 
+    @Test
+    fun loadReminders_loading() {
+        // GIVEN - loading reminders
+        //pause the dispatcher
+        mainCoroutineRule.pauseDispatcher()
+        //get reminders
+        remindersListViewModel.loadReminders()
 
+        // WHEN - the dispatcher is paused, showLoading is true
+        assertThat(remindersListViewModel.showLoading.getOrAwaitValue(), `is`(true))
+        mainCoroutineRule.resumeDispatcher()
 
+        // THEN - when the dispatcher is resumed, showboating is false
+        assertThat(remindersListViewModel.showLoading.getOrAwaitValue(), `is`(false))
+    }
 
 
 
--- starter/app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
+++ starter/app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
@@ -1,16 +1,110 @@
 package com.udacity.project4.locationreminders.savereminder
 
+import android.app.Application
+import android.os.Build
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import com.udacity.project4.locationreminders.MainCoroutineRule
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.hamcrest.CoreMatchers
+import org.hamcrest.MatcherAssert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
+import org.robolectric.annotation.Config
 
+@Config(sdk = [Build.VERSION_CODES.P])
+// Set the main coroutines dispatcher for unit testing.
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
 class SaveReminderViewModelTest {
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    // Set the main coroutines dispatcher for unit testing.
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    // Subject under test
+    private lateinit var saveReminderViewModel: SaveReminderViewModel
+
+    // Use a fake repository to be injected into the view model.
+    private lateinit var fakeDataSource: FakeDataSource
+
+    @Before
+    fun setupViewModel() {
+        stopKoin()
+        // Initialise the repository with no reminders.
+        fakeDataSource = FakeDataSource()
+        val appContext = ApplicationProvider.getApplicationContext() as Application
+        saveReminderViewModel = SaveReminderViewModel(appContext, fakeDataSource)
+    }
+
+    @Test
+    fun whenIncompleteInfo_validationReturnsTrue() {
+        // GIVEN
+        //  reminder fields if location and title  not null should return false
+        saveReminderViewModel.onClear()
+        saveReminderViewModel.reminderTitle.value = "location"
+        saveReminderViewModel.reminderDescription.value = "some description"
+        saveReminderViewModel.reminderSelectedLocationStr.value = "location"
+        saveReminderViewModel.longitude.value = 20.0
+        saveReminderViewModel.latitude.value = 10.0
+
+        // WHEN - attempting to validate
+        val result = saveReminderViewModel.validateEnteredData(
+            ReminderDataItem(
+                saveReminderViewModel.reminderTitle.value,
+                saveReminderViewModel.reminderDescription.value,
+                saveReminderViewModel.reminderSelectedLocationStr.value,
+                saveReminderViewModel.longitude.value,
+                saveReminderViewModel.latitude.value,
+                "2"
+            )
+        )
+
+        // THEN - result is false
+        // using hamcrest is for more readability
+        MatcherAssert.assertThat(result, CoreMatchers.`is`(true))
+
+    }
 
 
-    //TODO: provide testing to the SaveReminderView and its live data objects
+
+    @Test
+    fun whenIncompleteInfo_validationReturnsNull() {
+        // GIVEN
+        //  reminder fields if location  is null should return false
+        saveReminderViewModel.onClear()
+        saveReminderViewModel.reminderTitle.value = "location"
+        saveReminderViewModel.reminderDescription.value = "some description"
+        saveReminderViewModel.reminderSelectedLocationStr.value = null
+        saveReminderViewModel.longitude.value = 15.0
+        saveReminderViewModel.latitude.value = 10.0
+
+        // WHEN - attempting to validate
+        val result = saveReminderViewModel.validateEnteredData(
+            ReminderDataItem(
+                saveReminderViewModel.reminderTitle.value,
+                saveReminderViewModel.reminderDescription.value,
+                saveReminderViewModel.reminderSelectedLocationStr.value,
+                saveReminderViewModel.longitude.value,
+                saveReminderViewModel.latitude.value,
+                "1"
+            )
+        )
+
+        // THEN - result is false
+        // using hamcrest is for more readability
+        MatcherAssert.assertThat(result, CoreMatchers.`is`(false))
+
+    }
+
 
 
 }
